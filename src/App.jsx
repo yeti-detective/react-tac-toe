4f3e8ab92e34a8cc89cb727f4e4c05a8ef005703
Christopher Stardust <yeti.detective@proton.me>
2024-05-15 21:42:08 -0400

    personalize it, and nab the styles.css file from the tutorial site

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ReactLogo from "./ReactLogo";
+import "./App.css";
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <h2>
+        <ReactLogo />
+        Go to the{" "}
+        <a href="https://react.dev/learn/tutorial-tic-tac-toe" target="_blank">
+          tutorial
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <ReactLogo />
+      </h2>
+      <ul>
+        <li>We don't need to do the setup steps</li>
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/ReactLogo.jsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/react.svg";
+
+/**
+ * The React logo
+ * @returns JSX.Element
+ */
+export default function ReactLogo() {
+  return (
+    <img
+      src={reactLogo}
+      className="logo react"
+      alt="React logo"
+    />
+  );
+}
