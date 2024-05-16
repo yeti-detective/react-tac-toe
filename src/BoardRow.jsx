3be8e4bb288957c149400f940f4410adb111a3af
Chris B <yeti.detective@gmail.com>
2024-05-16 22:45:34 +0000

    format BoardRow.jsx

--- src/BoardRow.jsx
+++ src/BoardRow.jsx
@@ -1,10 +1,11 @@
-import Square from './Square';
+import Square from "./Square";
 
-export default function BoardRow({turn, reset}) {
-  
-  return <div className="board-row">
-    <Square turn={turn} reset={reset} />
-    <Square turn={turn} reset={reset} />
-    <Square turn={turn} reset={reset} />
-  </div>
+export default function BoardRow({ turn, reset }) {
+  return (
+    <div className="board-row">
+      <Square turn={turn} reset={reset} />
+      <Square turn={turn} reset={reset} />
+      <Square turn={turn} reset={reset} />
+    </div>
+  );
 }
